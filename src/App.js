5bcef22c4ed410c5f286e439aa51bced2d9e1a34
github username <github email address>
2019-11-12 21:44:14 -0500

    9:44 pm

--- src/App.js
+++ src/App.js
@@ -49,17 +49,21 @@ class App extends React.Component {
     
   }
 
-  componentDidUpdate = (prevProps, prevState) => {
-    // console.log('PREVOIUS', prevProps, prevState)
-    // console.log("CURRENT", this.props, this.state)
-    if(prevProps.location.pathname === '/newpoll' && this.props.location.pathname === '/polls'){
-      this.fetchAllPolls()
-    }
-    else if(prevState.allPolls !== this.state.allPolls){
-      this.fetchAllPolls()
-    }
-    // compare prevProps location path and this.props.location path and then do the fetch again 
-  }
+// -------------------------------------------NEED THIS DO NOT DELETE----------------------------------------------- //
+
+  // componentDidUpdate = (prevProps, prevState) => {
+  //   // console.log('PREVOIUS', prevProps, prevState)
+  //   // console.log("CURRENT", this.props, this.state)
+  //   if(prevProps.location.pathname === '/newpoll' && this.props.location.pathname === '/polls'){
+  //     this.fetchAllPolls()
+  //   }
+  //   else if(prevState.allPolls !== this.state.allPolls){
+  //     this.fetchAllPolls()
+  //   }
+  // }
+  //   // compare prevProps location path and this.props.location path and then do the fetch again 
+
+// -------------------------------------------NEED THIS DO NOT DELETE----------------------------------------------- //
 
   setUser = (user) => {
     // event.preventDefault();
